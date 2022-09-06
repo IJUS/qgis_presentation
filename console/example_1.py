102509c9d9b975f6f0210b9f0eb2a7d202d2c337
Bryan <54280556+bryanwofter@users.noreply.github.com>
2022-09-06 08:21:41 -0400

    Update example_1.py

--- console/example_1.py
+++ console/example_1.py
@@ -1,7 +1,9 @@
-def createLayer_from_csv(csvPath, x_field, y_field, layerName):
-    # create map vector layer from csv xy values
+def createLayer_from_csv(csvPath, x_field, y_field, layer_name):
+    """
+    Create a vector layer from the given CSV file, x field, y field, and layer name.
+    """
     uri = f"file:///{csvPath}?delimiter=,&yField={y_field}&xField={x_field}"
-    return QgsVectorLayer(uri, layerName, 'delimitedtext')
+    return QgsVectorLayer(uri, layer_name, 'delimitedtext')
     
 layer_csv = createLayer_from_csv('C:/Temp/camera_locations.csv', 'lng', 'lat', 'locations')
 QgsProject.instance().addMapLayer(layer_csv)
